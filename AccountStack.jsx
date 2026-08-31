abb20f22b792ece7df09f2c0c2d898becb68c8bc
Pramodya Senadheera <108608394+pramodya98@users.noreply.github.com>
2026-08-31 10:40:46 +0530

    FE-T2-10
    FE-T2-10: connect Dietary Requirements page to backend

---
+++ AccountStack.jsx
@@ -0,0 +1,62 @@
+import { createStackNavigator } from "@react-navigation/stack";
+
+import DeleteAccountScreen from "../screens/profile/DeleteAccountScreen";
+import EditProfileScreen from "../screens/profile/EditProfileScreen";
+import ProfileScreen from "../screens/profile/ProfileScreen";
+import SettingsScreen from "../screens/profile/SettingsScreen";
+import TimerScreen from "../screens/tools/TimerScreen";
+import DietaryRequirementsScreen from "../screens/account/DietaryRequirementsScreen";
+import PlaceholderScreen from "./_PlaceholderScreen";
+
+const Stack = createStackNavigator();
+export default function AccountStack() {
+  return (
+    <Stack.Navigator initialRouteName="ProfileScreen">
+      <Stack.Screen
+        name="ProfileScreen"
+        component={ProfileScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="EditProfileScreen"
+        component={EditProfileScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SettingsScreen"
+        component={SettingsScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="DeleteAccountScreen"
+        component={DeleteAccountScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="DietaryRequirementsScreen"
+        component={DietaryRequirementsScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ShoppingListScreen"
+        component={PlaceholderScreen}
+        options={{ title: "Shopping List" }}
+      />
+      <Stack.Screen
+        name="AppointmentsScreen"
+        component={PlaceholderScreen}
+        options={{ title: "Appointments" }}
+      />
+      <Stack.Screen
+        name="TimerScreen"
+        component={TimerScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="HealthToolsScreen"
+        component={PlaceholderScreen}
+        options={{ title: "Health Tools" }}
+      />
+    </Stack.Navigator>
+  );
+}
